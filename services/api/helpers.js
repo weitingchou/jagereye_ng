cec727e865a69cd0b6748c852c54566a84d80e21
weitingchou <wtchou1209@gmail.com>
2018-04-18 10:26:33 +0800

    Add timeout to getStreamMetadata()

--- services/api/helpers.js
+++ services/api/helpers.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const { createError } = require('./utils')
+const find = require('lodash/find');
 
 const ffmpeg = require('fluent-ffmpeg');
 const Promise = require('bluebird');
 
-const find = require('lodash/find');
-
-const ffprobe = Promise.promisify(ffmpeg.ffprobe);
+const TIMEOUT_PERIOD = 30000
 
 
 function getStreamMetadata(req, res, next) {
     const url = req.query.url
-    return ffprobe(url).then(metadata => {
+    let isTimeout = false
+
+    const timeout = setTimeout(() => {
+        isTimeout = true
+        next(createError(404, null))
+    }, TIMEOUT_PERIOD)
+
+    ffmpeg.ffprobe(url, (err, metadata) => {
+        if (isTimeout) { return }
+        else { clearTimeout(timeout) }
+
+        if (err) { return next(createError(404, null, err)) }
+
         let width
         let height
         let stream
@@ -29,14 +41,15 @@ function getStreamMetadata(req, res, next) {
             ))
 
             if (!stream) {
-                throw 'Can not get video size'
+                // XXX: Does 400 make sense?
+                return next(createError(400, 'Unable to get video frame size'))
             }
 
             width = stream.coded_width
             height = stream.coded_height
         }
 
-        res.send({width, height})
+        return res.send({width, height})
     })
 }
 
